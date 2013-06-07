73758b6c034aeba65c680b9b0cd482cf8f121bf7
napodan <david.guenet@gmail.com>
2013-06-07 22:59:45 +0200

    Remove all products but bravo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,1 @@
 add_lunch_combo oxygen_bravo-user
-add_lunch_combo oxygen_bravoc-user
-add_lunch_combo oxygen_crespo-user
-add_lunch_combo oxygen_crespo4g-user
-add_lunch_combo oxygen_galaxys2-user
-add_lunch_combo oxygen_passion-user
